750be20e5dbb603d5a9adcfd4b1faa40ce6e118a
Trevor Santoli <trevorichi@gmail.com>
2022-06-24 17:55:20 -0400

    Got a function working and some loops, now lets move onto some bigger stuff

--- BasicLearning/HelloWorld.py
+++ BasicLearning/HelloWorld.py
@@ -1,4 +1,18 @@
+from re import A
+
+
 print("Hello World")
 
 for i in range(10):
     print("Testing for loop")
+
+
+def howDoFunctionsWork(printMe):
+    print("testing function")
+    print(printMe)
+
+a = "Hello world, but from a function"
+
+howDoFunctionsWork(a)
+
+
